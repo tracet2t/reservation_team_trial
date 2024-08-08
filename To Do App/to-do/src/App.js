f56fad44727e17e4ab3a2e41b6d5c2147e76c971
VIHARI7 <dviha7@gmail.com>
2024-08-09 02:56:32 +0530

    do some test cases

--- To Do App/to-do/src/App.js
+++ To Do App/to-do/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/todo';
 
 function App() {
+  const todos = [
+  {id: 1, title: 'task01', completed: false, },
+  {id: 2, title: 'task 02', completed: true, },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="App">
+      {todos.map((todo) => {
+      return (<Todo todo={todo}/>)
+      })}
     </div>
   );
 }
