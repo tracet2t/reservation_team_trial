cc8b236b73225b2aa8a5d2ee5b94bef8e0ee0b57
VIHARI7 <dviha7@gmail.com>
2024-08-16 17:17:34 +0530

    update dependencies

--- To Do App/to-do-app/frontend/src/App.js
+++ To Do App/to-do-app/frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
+import EditTask from './components/EditTask';
+import Auth from './components/Auth';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>To-Do App</h1>
-        <AddTask />
-      <TaskList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/add" element={<AddTask />} />
+        <Route path="/edit/:id" element={<EditTask />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </Router>
   );
 }
 
