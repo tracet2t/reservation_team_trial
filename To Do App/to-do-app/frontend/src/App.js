6ba195cbfdd7e5512081801de868245a8b2bb7d5
VIHARI7 <dviha7@gmail.com>
2024-08-17 15:46:41 +0530

    Setup routes

--- To Do App/to-do-app/frontend/src/App.js
+++ To Do App/to-do-app/frontend/src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
+import EditTask from './components/EditTask';
+import Auth from'./components/Auth';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <h1>To-Do App</h1>
-        <AddTask />
-      <TaskList />
-    </div>
+    <Router>
+      <div className="App">
+      <h1>To-Do App</h1>
+          <Routes>
+            <Route path="/"  element={
+              <div className="app-container">
+                <div className="add-task-container">
+                <AddTask />
+                </div>
+                <div className="task-list-container">
+                  <TaskList />
+                  </div>
+              </div>
+              }
+            />
+            <Route path="/editTask/:id" element={<EditTask />} />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
--- To Do App/to-do-app/frontend/src/App.test.js
+++ To Do App/to-do-app/frontend/src/App.test.js
@@ -7,3 +7,17 @@ test('renders To-Do App title', () => {
   const titleElement = screen.getByText(/To-Do App/i);
   expect(titleElement).toBeInTheDocument();
 });
+// Check AddTask component
+test('renders AddTask component', () => {
+  render(<App />);
+  const addTaskElement = screen.getByText(/Add Task/i); 
+  expect(addTaskElement).toBeInTheDocument();
+});
+
+// Check TaskList component
+test('renders TaskList component', () => {
+  render(<App />);
+  const taskListElement = screen.getByText(/All Tasks/i);
+  expect(taskListElement).toBeInTheDocument();
+});
+
