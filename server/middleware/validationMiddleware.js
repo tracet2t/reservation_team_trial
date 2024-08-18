22b963c333a4230b7bd29ae7f2b931c829639522
AM-ASKY-97 <am.asky97@gmail.com>
2024-08-19 01:04:16 +0530

    I have create CRUD function for the todo

--- server/middleware/validationMiddleware.js
+++ server/middleware/validationMiddleware.js
@@ -1,14 +1,35 @@
-export function validationMiddleware(schema) {
-  return (req, res, next) => {
-    try {
-      schema.parse(req.body);
-      next();
-    } catch (err) {
-      const formattedErrors = err.errors.map((error) => ({
-        path: error.path.join('.'),
-        message: error.message,
-      }));
-      res.status(400).json({ errors: formattedErrors });
-    }
-  };
-}
+// /middleware/validationMiddleware.js
+import Joi from 'joi';
+import { createValidator } from 'express-joi-validation';
+
+const validator = createValidator({});
+
+const userSchema = Joi.object({
+  title: Joi.string().min(5).required().messages({
+    'string.base': 'Title should be a type of text',
+    'string.empty': 'Title cannot be an empty field',
+    'string.min': 'Title should have a minimum length of {#limit}',
+    'any.required': 'Title is a required field'
+  }),
+  dueDate: Joi.date().required().messages({
+    'date.base': 'Due Date should be a valid date',
+    'date.empty': 'Due Date cannot be an empty field',
+    'any.required': 'Due Date is a required field'
+  }),
+  description: Joi.string().min(8).required().messages({
+    'string.base': 'Description should be a type of text',
+    'string.empty': 'Description cannot be an empty field',
+    'string.min': 'Description should have a minimum length of {#limit}',
+    'any.required': 'Description is a required field'
+  }),
+  priority: Joi.string().valid('low', 'medium', 'high').required().messages({
+    'string.base': 'Priority should be a type of text',
+    'any.only': 'Priority must be one of [low, medium, high]',
+    'string.empty': 'Priority cannot be an empty field',
+    'any.required': 'Priority is a required field'
+  })
+});
+
+const validateUser = validator.body(userSchema);
+
+export { validateUser };
--- server/routes/TodoRoute.js
+++ server/routes/TodoRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { store, testRoute } from '../controller/TodoController.js';
-import { validationMiddleware } from '../middleware/validationMiddleware.js';
-import { TodoSchema } from '../schema/TodoSchema.js';
+import { deleteById, findById, store, update } from '../controller/TodoController.js';
+import { validateUser } from '../middleware/validationMiddleware.js';
 
 const router = express.Router();
 
-router.get('/test', testRoute);
-router.post('/store', validationMiddleware(TodoSchema), store);
+// router.get('/test', testRoute);
+router.post('/store', validateUser, store);
+router.get('/todos/:id', findById);
+router.put('/todos/:id', update);
+router.delete('/todos/:id', deleteById);
 
 export default router;
