05127ebc1480ff3a304854b1e803c616e65d6fad
Mohamed shazan <shaloste@gmail.com>
2024-08-21 19:20:08 +0530

    I have deleted

--- Backend/schema/TodoSchema.js
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const TodoSchema = z.object({
-  title: z.string().min(1, { message: "title is required" }),
-  dueDate : z.string().min(1, { message: "Due date is required" }),
-});
